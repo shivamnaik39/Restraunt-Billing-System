199e34ed2a58ac2acfc923e640dcc31e712ebd95
Shivam Naik <shivamnaik8239@gmail.com>
2019-10-02 17:43:28 +0530

    added warnings on negative inppts

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // box-1
 billno = document.querySelector("#billno");
 var menu = document.querySelectorAll("#items .menu");
+menu = Array.from(menu);
 
 // box-2
 var price = document.querySelector("#price");
@@ -11,8 +12,13 @@ var Tprice = document.querySelector("#Tprice");
 var resetb = document.querySelector("#resetb");
 var calculateb = document.querySelector("#calculateb");
 
+
 // box-3
 var rates = document.querySelectorAll("#pricing .rates");
+rates = Array.from(rates);
+
+var stocks = document.querySelectorAll("#stock .stocks");
+stocks = Array.from(stocks);
 
 
 // Nav-buttons
@@ -23,10 +29,29 @@ var PRICINGb = document.querySelector("#PRICING");
 var page1 = document.querySelector("#page1");
 var page2 = document.querySelector("#page2");
 
+// warning 1
+var warn1 = document.querySelector("#warning1");
+
 
 
 
 // Functions
+
+function isNegative() {
+    for (var i = 0; i < menu.length; i++) {
+        var im = parseInt(menu[i].value, 10);
+        var ir = parseInt(rates[i].value, 10);
+        var is = parseInt(stocks[i].value, 10);
+        var ivat = parseInt(vat.value, 10);
+
+        if (im < 0 || ir < 0 || is < 0 || ivat < 0)
+            return 1;
+
+    }
+    return 0;
+}
+
+
 
 function reset() {
     bn = parseInt(billno.value, 10);
@@ -35,12 +60,22 @@ function reset() {
 
     billno.value = bn.toString(10);
 
-    for (var i = 0; i < menu.length; i++)
+    for (var i = 0; i < menu.length; i++) {
         menu[i].value = "0";
+        menu[i].style.background = "#ffffff";
+    }
+
 
 
     price.value = Tprice.value = "";
 
+    if (isNegative()) {
+        warn1.style.display = "block";
+    }
+    else {
+        warn1.style.display = "none";
+
+    }
 
 }
 
@@ -48,6 +83,10 @@ function reset() {
 function billing() {
 
     var sum = 0;
+
+    if (isNegative()) {
+        return 0;
+    }
 
     for (var i = 0; i < menu.length; i++)
         sum += menu[i].value * rates[i].value;
@@ -88,7 +127,9 @@ function pricingf() {
 
 
 
+
 // Procedure
+
 
 resetb.addEventListener("click", reset);
 calculateb.addEventListener("click", calculate);
@@ -96,3 +137,23 @@ calculateb.addEventListener("click", calculate);
 HOMEb.addEventListener("click", homef);
 
 PRICINGb.addEventListener("click", pricingf);
+
+var ipArray = [vat];
+ipArray = ipArray.concat(menu, rates, stocks);
+
+for (var i = 0; i < ipArray.length; i++) {
+
+    ipArray[i].addEventListener("change", function () {
+        var ia = parseInt(this.value, 10);
+        if (ia < 0) {
+            warn1.textContent = " Neagtive values have been entered in some fields!!";
+            this.style.background = "#ff7070";
+            warn1.style.display = "block";
+
+        }
+        else {
+            warn1.style.display = "none";
+            this.style.background = "#ffffff";
+        }
+    });
+}
